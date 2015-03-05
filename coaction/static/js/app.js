3135e9f405222c06d88fc6da3df8cea227822cbe
Paget Fink <pagetfink@gmail.com>
2015-03-05 17:19:56 -0500

    add new task page working

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -11,6 +11,48 @@ app.config(['$routeProvider', function ($routeProvider) {
   });
 }]);
 
+app.config(['$routeProvider', function($routeProvider) {
+  var routeDefinition = {
+    templateUrl: 'static/new-task/new-task.html',
+    controller: 'NewTaskCtrl',
+    controllerAs: 'vm'
+  };
+
+  $routeProvider.when('/tasks/new', routeDefinition);
+}])
+.controller('NewTaskCtrl', ['Task', function (Task) {
+
+  var self = this;
+
+  self.newTask = Task();
+
+  self.addTask = function () {
+
+    // Make a copy of the 'newTask' object
+    var task = Task(self.newTask);
+
+    // Add the task to our service
+    tasksService.addTask(task);
+
+    self.newTask = Task();
+
+  };
+
+}]);
+
+app.factory('Task', function () {
+  return function (spec) {
+    spec = spec || {};
+    return {
+      id: spec.id || '',
+      title: spec.title || '',
+      description: spec.description || '',
+      status: spec.status || '',
+      due_date: spec.due_date || ''
+    };
+  };
+});
+
 app.controller('Error404Ctrl', ['$location', function ($location) {
   this.message = 'Could not find: ' + $location.url();
 }]);
