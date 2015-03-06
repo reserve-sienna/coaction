52858d37278d7dd61abd75d51a644efc6e3c0685
Ana Maria Echeverri <ame@anamariaecheverri.com>
2015-03-06 13:16:36 -0500

    added login model, form and code. Not tested yet.

--- coaction/models.py
+++ coaction/models.py
@@ -1,7 +1,13 @@
-from .extensions import db
+from .extensions import db, bcrypt, login_manager
 from marshmallow import Schema, fields, ValidationError
+from flask.ext.login import UserMixin
+from sqlalchemy import func, and_
+from datetime import date, timedelta, datetime
+from flask import request, url_for
 
-"""Add your models here."""
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
 
 
 class Task(db.Model):
@@ -26,3 +32,27 @@ def must_not_be_blank(data):
         raise ValidationError("Data not provided.")
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    encrypted_password = db.Column(db.String(60))
+
+    def get_password(self):
+        return getattr(self, "_password", None)
+
+    def set_password(self, password):
+        self._password = password
+        self.encrypted_password = bcrypt.generate_password_hash(password)
+
+    password = property(get_password, set_password)
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.encrypted_password, password)
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
+
+class UserSchema(Schema):
+    class Meta:
+        fields = ("id", "name", "email")
--- coaction/views.py
+++ coaction/views.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, flash, jsonify, request
-from .models import Task, TaskSchema
-from .forms import TaskForm
+from .models import Task, TaskSchema, User, UserSchema
+from .forms import TaskForm, LoginForm, RegistrationForm
 from .extensions import db
+from flask.ext.login import login_user, logout_user, current_user
+
 
 coaction = Blueprint("coaction", __name__, static_folder="./static")
 
@@ -28,6 +30,7 @@ def tasks():
 def add_task():
     task_data = request.get_json()
     form = TaskForm(data=task_data)
+    form.status.data = "New"
     if form.validate():
         task = Task(**form.data)
         db.session.add(task)
@@ -81,5 +84,47 @@ def delete_task(id):
         return jsonify({"status": "fail", "data": {"title": "Could not delete."}}), 400
 
 
+@coaction.route("/api/login", methods=["POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            serializer = UserSchema()
+            result = serializer.dump(user)
+            return jsonify({"status": "success",
+                            "data": result.data})
+        else:
+            return jsonify({"status": "fail", "data": {"title": "Could not login user."}}), 400
 
+
+@coaction.route("/api/logout", methods=["POST"])
+def logout():
+    user = current_user
+    logout_user()
+    serializer = UserSchema()
+    result = serializer.dump(user)
+    return jsonify({"status": "success",
+                    "data": result.data})
+
+
+@coaction.route("/api/register", methods=["POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            return jsonify({"status": "fail", "data": {"title": "This user already exists"}}), 400
+        else:
+            user = User(name=form.name.data,
+                        email=form.email.data,
+                        password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            serializer = UserSchema()
+            result = serializer.dump(user)
+            return jsonify({"status": "success",
+                    "data": result.data})
 
