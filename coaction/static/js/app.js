83348c1a5c423ee1247033e2aee0fb7d99949afb
hifilorau <tomrau@gmail.com>
2015-03-06 11:47:34 -0500

    check in pull

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -11,6 +11,48 @@ app.config(['$routeProvider', function ($routeProvider) {
   });
 }]);
 
+app.config(['$routeProvider', function($routeProvider) {
+  var routeDefinition = {
+    templateUrl: 'static/new-task/new-task.html',
+    controller: 'NewTaskCtrl',
+    controllerAs: 'vm'
+  };
+
+  $routeProvider.when('/tasks/new', routeDefinition);
+}])
+.controller('NewTaskCtrl', ['Task', function (Task) {
+
+  var self = this;
+
+  self.newTask = Task();
+
+  self.addTask = function () {
+
+    // Make a copy of the 'newTask' object
+    var task = Task(self.newTask);
+
+    // Add the task to our service
+    tasksService.addTask(task);
+
+    self.newTask = Task();
+
+  };
+
+}]);
+
+app.factory('Task', function () {
+  return function (spec) {
+    spec = spec || {};
+    return {
+      id: spec.id || '',
+      title: spec.title || '',
+      description: spec.description || '',
+      status: spec.status || '',
+      due_date: spec.due_date || ''
+    };
+  };
+});
+
 app.factory('tasksService', ['$http', '$log', function($http, $log) {
 
   function get(url) {
